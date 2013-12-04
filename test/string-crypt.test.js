141455b6f070e7ea6becf29c34569517ddb05d7a
Adam Duncan <adam.duncan@clock.co.uk>
2013-12-04 21:13:25 +0000

    Broken

--- test/string-crypt.test.js
+++ test/string-crypt.test.js
@@ -4,7 +4,7 @@ describe('string-crypt', function () {
 
   it('should correctly encrpyt a string ', function () {
     var encrpytedString = stringCrypt('hello', 'mySecretKey')
-    encrpytedString.should.equal('7372493cd3e2b8ecb668879af45496d539559731')
+    encrpytedString.should.equal('BROKEN')
   })
 
 })
